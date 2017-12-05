b50222c71d7ddaca2e9cd1a17863528f56ad3c08
KaiYuan•Guo <KaiYuan.Guo@gmail.com>
2017-12-06 00:13:29 +0800

    Add STM32F103VC model support.

---
+++ Modules/STM32/F1/STM32F103VC.cmake
@@ -0,0 +1,23 @@
+add_definitions ( -DSTM32F103xE ) #Add chip marco.
+
+if ( NOT STM32_RAM_START_ADDRESS )
+	set ( STM32_RAM_START_ADDRESS "0x20000000" )
+endif ()
+
+if ( NOT STM32_ROM_START_ADDRESS )
+	set ( STM32_ROM_START_ADDRESS "0x08000000" )
+endif ()
+
+if ( NOT STM32_MIN_HEAP_SIZE )
+	set ( STM32_MIN_HEAP_SIZE "0" )
+endif ()
+
+if ( NOT STM32_MIN_STACK_SIZE )
+	set ( STM32_MIN_STACK_SIZE "0x400" )
+endif ()
+
+set ( STM32_RAM_SIZE 48K )
+set ( STM32_ROM_SIZE 256K )
+set ( STM32_RAM_END_ADDRESS "0x2000C000" )
+
+set ( STM32_STARTUP_SOURCE_NAME startup_stm32f103xb.s )
